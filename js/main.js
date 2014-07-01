35bc76ac337c77f592dab9b097d8c281b9f8eb7d
Continuous Integration [CI] Outright Mental Inc <ci@outrightmental.com>
2014-07-01 16:30:46 -0400

    Using index.html from HTML5 Boilerplate, and simpler to reference all vendor libraries at their CDN.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
 
+// Activates the Carousel
+$('.carousel').carousel({
+  interval: 5000
+})
+
+// Activates Tooltips for Social Links
+$('.tooltip-social').tooltip({
+  selector: "a[data-toggle=tooltip]"
+})
